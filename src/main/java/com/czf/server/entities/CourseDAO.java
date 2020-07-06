78c5df31b8d607cc45a4f753a08d9309661317f9
czf <1354016594@qq.com>
2020-07-06 15:31:18 +0800

    Revert "完成数据库model的搭建"
    This reverts commit 3e44d13bbd2b6d62a0f66d93b4bffe9f281ef21b.

--- src/main/java/com/czf/server/entities/CourseDAO.java
+++
@@ -1,6 +0,0 @@
-package com.czf.server.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDAO extends JpaRepository<Course,Integer> {
-}
--- src/main/java/com/czf/server/entities/StudentDAO.java
+++
@@ -1,6 +0,0 @@
-package com.czf.server.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentDAO extends JpaRepository<Student,Integer> {
-}
